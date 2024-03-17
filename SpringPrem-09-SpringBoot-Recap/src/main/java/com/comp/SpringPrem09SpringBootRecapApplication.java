2990debc285b14b47dd27801e6c0faed087c3a3e
omerb <hannanorhn00@gmail.com>
2024-03-16 22:40:16 -0700

    SpringBoot deleted and added again

--- SpringPrem-09-SpringBoot-Recap/src/main/java/com/comp/SpringPrem09SpringBootRecapApplication.java
+++ SpringPrem-09-SpringBoot-Recap/src/main/java/com/comp/SpringPrem09SpringBootRecapApplication.java
@@ -1,4 +1,4 @@
-package com.comp.springprem09springbootrecap;
+package com.comp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringPrem09SpringBootRecapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringPrem09SpringBootRecapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPrem09SpringBootRecapApplication.class, args);
+    }
 
 }
